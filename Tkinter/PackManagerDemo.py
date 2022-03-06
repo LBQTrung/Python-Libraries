9f8d483dba24d9c03e2e6c9e5e9c425cf702c631
LBQTrung <91483513+LBQTrung@users.noreply.github.com>
2022-03-06 21:14:52 +0700

    Add pack, place manager demo

---
+++ Tkinter/PackManagerDemo.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+class PackManagerDemo:
+    def __init__(self):
+        window = Tk()
+        window.title("Pack Manager Demo")
+
+        Label(window, text = "Blue", bg = "blue").pack()
+        Label(window, text = "Red", bg = "red").pack(
+                fill = BOTH, expand = 1
+        )
+        Label(window, text = "Red", bg = "red").pack(
+                fill = BOTH
+        )
+
+        window.mainloop()
+
+PackManagerDemo()
